dd69d1584e58ae440d8f428580f2d2a0e1fffa4a
danteMesquita <dantemesquitaneto@gmail.com>
2019-04-17 21:52:11 -0300

    Adicionado ConexaoMongo

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import * as express from 'express';
 import * as graphqlHTTP from 'express-graphql';
 import schema from './util/graphql/schema';
+import * as mongodb from 'mongodb';
 
 class App {
     public express: express.Application;
+    public mongodb: mongodb.MongoClient
 
     constructor(){
+        this.mongodb = new mongodb.MongoClient("mongodb+srv://root:root@cluster0-6ltor.mongodb.net/test?retryWrites=true",
+            {
+                useNewUrlParser: true,
+            }
+        );
         this.express = express();
         this.middleware();
     }
