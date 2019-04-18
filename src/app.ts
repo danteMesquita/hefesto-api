a42ffa15be4505402c1c24f7af05623403f58416
Dante Mesquita <dante.mesquita@tafner>
2019-04-18 14:35:27 -0300

    Adicionado Model Teste

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import * as express from 'express';
 import * as graphqlHTTP from 'express-graphql';
-import schema from './util/graphql/schema';
+import schema from './graphql/schema';
 import * as mongodb from 'mongodb';
+import * as bodyParser from 'body-parser';
 
 class App {
     public express: express.Application;
-    public mongodb: mongodb.MongoClient
+    public mongodb: mongodb.MongoClient;
 
     constructor(){
         this.mongodb = new mongodb.MongoClient("mongodb+srv://root:root@cluster0-6ltor.mongodb.net/test?retryWrites=true",
@@ -13,15 +14,27 @@ class App {
                 useNewUrlParser: true,
             }
         );
+        
         this.express = express();
+        this.express.use(bodyParser.json());
+        this.express.use(bodyParser.urlencoded({extended: false}));
+
         this.middleware();
     }
 
     private middleware(): void {
+        // http://api.hefesto.com/
+        this.express.get("/", (req, res)=>{
+            res.send("Silence is golden")
+        })
+
+        // http://api.hefesto.com/graphql
         this.express.use("/graphql", graphqlHTTP({
             "schema" : schema,
             "graphiql" : process.env.NODE_ENV === 'development'
         }))
+
+        
     }
 
 }
